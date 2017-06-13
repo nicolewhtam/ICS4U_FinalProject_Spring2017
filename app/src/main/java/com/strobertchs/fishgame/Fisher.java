9fecad9b4793bb79b2f86c6e4ba7aa5dcdc8ddac
AlexTan <senyuan.tan16@ycdsbk12.ca>
2017-06-13 11:10:03 -0400

    Fisher class complete

---
+++ app/src/main/java/com/strobertchs/fishgame/Fisher.java
@@ -0,0 +1,36 @@
+package com.strobertchs.fishgame;
+
+import android.graphics.Canvas;
+
+/**
+ * Created by Alex on 6/13/2017.
+ */
+
+public class Fisher extends AnimatedSprite {
+    public Fisher (int screen_width){
+        super();
+        setWidth(screen_width/15);
+        setPositionX(screen_width/2);
+        setPositionY(10);
+
+        setHorizontal_amount(20);
+    }
+
+    public void draw(Canvas source_canvas){
+        source_canvas.drawRect(getPositionX(),getPositionY(),getPositionX()+getWidth(), getPositionY() + getWidth(),paint);
+    }
+
+    public void stop(){
+        setMovingLeft(false);
+        setMovingRight(false);
+    }
+
+    public void updatePosition(){
+        if(isMovingRight()){
+            setPositionX(getPositionX() + getHorizontal_amount());
+        }
+        if(isMovingLeft()){
+            setPositionX(getPositionX() - getHorizontal_amount());
+        }
+    }
+}
